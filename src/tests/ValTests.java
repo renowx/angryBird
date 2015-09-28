43bff64b023444e1f930c020bfb67bb894fd1300
valentin KANCURZEWSKI <v.kancurzewski@etudiant.univ-lille1.fr>
2015-09-28 15:17:05 +0200

    test

--- src/tests/ValTests.java
+++ src/tests/ValTests.java
@@ -1,5 +1,36 @@
 package tests;
 
-public class ValTests {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.nio.Buffer;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ValTests extends JPanel {
+	public JFrame frame;
+    public JPanel panel;
+
+    public ValTests(){
+        frame=new JFrame();
+        panel=new JPanel();
+
+        panel.setSize(500,500);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.add(panel);
+        panel.repaint();
+    }
+
+    public void paint(Graphics g){
+        super.paintComponents(g);
+        g.drawOval(100, 200, 50, 50);
+    }
+
+    public static void main(String args[]){
+        new ValTests();
+    }
+	
+	
+	
 }
