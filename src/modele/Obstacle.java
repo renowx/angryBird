49ffa9e06a42da1625b2e36d984aa4a57f3c59d6
RenowxSiders <renaud.carpentier@etudiant.univ-lille.fr>
2015-09-28 13:57:50 +0200

    1er commit
    Renaud Carpentier
    
    ajout getter/setter

--- src/modele/Obstacle.java
+++ src/modele/Obstacle.java
@@ -17,10 +17,26 @@ public class Obstacle {
     private int y;
     
     public Obstacle(int x, int y){
-        this.x=x;
-        this.y=y;
+        this.setX(x);
+        this.setY(y);
         
     }
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
     
     
 }
