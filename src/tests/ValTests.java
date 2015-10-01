804b0722ab796b3866e4cc2108432de993fb7819
kancurzv <valentin.kancurzewski@hotmail.com>
2015-10-01 16:42:13 +0200

    Courbe polynomiale + arret obstacle

--- src/tests/ValTests.java
+++ src/tests/ValTests.java
@@ -1,47 +1,66 @@
 package tests;
 
-import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class ValTests extends JFrame {
 
 	private Panneau pan = new Panneau();
+	JFrame frame = new JFrame();
 
 	public ValTests() {
-		this.setTitle("Animation");
-		this.setSize(600, 400);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setContentPane(pan);
-		this.setVisible(true);
+		frame.setTitle("Angry_Bird");
+		frame.setSize(1200, 910);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setContentPane(pan);
+		frame.setVisible(true);
 		go();
 	}
 
 	private void go() {
-		for (int i = -50; i < pan.getWidth(); i++) {
-			int x = pan.getPosX()/*, y = pan.getPosY()*/;
-			x++;
-			//y++;
+		int x = pan.getPosX(), y = pan.getPosY();
+
+		for (int i = 0; i < pan.getWidth(); i++) {
+			x = pan.getPosX();
+			y = pan.getPosY();
+			x = x + 2;
+			y = (int) Parabole(x, y);
 			pan.setPosX(x);
-			//pan.setPosY(y);
-			pan.repaint();
+			pan.setPosY(y);
+			frame.repaint();
 			repaint();
+
+			// Vitesse de deplacement pour les tests passage au Timer par la
+			// suite
 			try {
-				//Rapidité du déplacement
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//Arrete arrive au bord de la feunetre
-			if(x == pan.getWidth()-50 /*|| y == pan.getHeight()-50*/){
-				i=pan.getWidth();
-				} 
+			// Arret de l'objet a ameliorer
+			if (x == frame.getWidth() - 100 || y == frame.getWidth() - 100
+					|| x == frame.getHeight() - 100
+					|| y == frame.getHeight() - 100
+					|| (x >= pan.getPosXo2() - 43 && y >= pan.getPosYo2() - 43)
+					|| x <= 0 || y <= 0) {
+				i = pan.getWidth();
+			}
 		}
 	}
-	
-	public static void main(String[] args){
+
+	// Equation d'une parabole
+	private float Parabole(float x, float y) {
+		y = (float) (-((0.005) * (x * x)) + 4 * x + 2);
+		return y;
+	}
+
+	private double Parabole(double a, double x) {
+		return a * Math.pow(x, 2); // Math.pow(x,2) vaut x2
+	}
+
+	public static void main(String[] args) {
 		new ValTests();
 	}
-	
 
 }
