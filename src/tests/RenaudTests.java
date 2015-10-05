f12908daf7c48f185b6dd74fd92d45eaf7536ce9
RenowxSiders <renaud.carpentier@etudiant.univ-lille.fr>
2015-10-05 15:07:12 +0200

    Timer
    Ajout de Timer, plus de sleep !

--- src/tests/RenaudTests.java
+++ src/tests/RenaudTests.java
@@ -2,43 +2,43 @@ package tests;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
-import javax.swing.Timer;
 
 public class RenaudTests extends JFrame {
 
 	private Panneau pan = new Panneau();
 	JFrame frame = new JFrame();
-	
+	Timer time = new Timer();
 	
 	public RenaudTests() {
+		
 		frame.setTitle("Angry_Bird");
-		frame.setSize(1200, 910);
+		frame.setSize(1200, 660);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setContentPane(pan);
 		frame.setVisible(true);
-		go();
+		int delay = 10; //milliseconds
+		TimerTask task = new TimerTask() {		
+			@Override
+			public void run() {	
+				go();		
+			}
+		};
+		
+		time.schedule(task, delay, 10);
 		
 	}
 
 	private void go() {
 		int x = pan.getPosX(), y = pan.getPosY();
-		Timer timer = new Timer(10, new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		for (int i = 0; i < pan.getWidth(); i++) {
 			x = pan.getPosX();
 			y = pan.getPosY();
-			x = x + 2;
+			x = x + 1;
 			y = (int) Parabole(x, y);
 			pan.setPosX(x);
 			pan.setPosY(y);
@@ -47,20 +47,14 @@ public class RenaudTests extends JFrame {
 
 			// Vitesse de deplacement pour les tests passage au Timer par la
 			// suite
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 			// Arret de l'objet a ameliorer
+			
 			if (x == frame.getWidth() - 100 || y == frame.getWidth() - 100
 					|| x == frame.getHeight() - 100
 					|| y == frame.getHeight() - 100
-					|| (x >= pan.getPosXo2() - 43 && y >= pan.getPosYo2() - 43)
+					|| (x >= pan.getPosXo2() - 43 && y <= pan.getPosYo2() - 43)
 					|| x <= 0 || y <= 0) {
-				i = pan.getWidth();
 			}
-		}
 	}
 
 	// Equation d'une parabole
