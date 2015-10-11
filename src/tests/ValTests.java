70a565d1f49284d5f667a4a6d9f8d1595ddfb634
kancurzv <valentin.kancurzewski@hotmail.com>
2015-10-11 14:11:26 +0200

    Direction courbe + deplace obstacle au touché

--- src/tests/ValTests.java
+++ src/tests/ValTests.java
@@ -1,11 +1,16 @@
 package tests;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JFrame;
 
 public class ValTests extends JFrame {
 
 	private Panneau pan = new Panneau();
 	JFrame frame = new JFrame();
+	Timer time = new Timer();
+	int x = pan.getPosX(), y = pan.getPosY();
 
 	public ValTests() {
 		frame.setTitle("Angry_Bird");
@@ -15,13 +20,22 @@ public class ValTests extends JFrame {
 		frame.setResizable(false);
 		frame.setContentPane(pan);
 		frame.setVisible(true);
-		go();
+		int delay = 10; // milliseconds
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				go();
+			}
+		};
+
+		time.schedule(task, delay, 10);
+
 	}
 
 	private void go() {
 		int x = pan.getPosX(), y = pan.getPosY();
 
-		for (int i = 0; i < pan.getWidth(); i++) {
+		while (!(x >= pan.getPosXo2() - 25 && y <= pan.getPosYo2() - 25)) {
 			x = pan.getPosX();
 			y = pan.getPosY();
 			x = x + 2;
@@ -42,16 +56,26 @@ public class ValTests extends JFrame {
 			if (x == frame.getWidth() - 100 || y == frame.getWidth() - 100
 					|| x == frame.getHeight() - 100
 					|| y == frame.getHeight() - 100
-					|| (x >= pan.getPosXo2() - 43 && y >= pan.getPosYo2() - 43)
-					|| x <= 0 || y <= 0) {
-				i = pan.getWidth();
+					|| (x == pan.getPosXo2() && y == pan.getPosYo2()) || x <= 0
+					|| y <= 0) {
+
 			}
 		}
+
+		x = pan.getPosXo2();
+		y = pan.getPosYo2();
+		x = x + 2;
+		y = (int) Parabole(x, y);
+		pan.setPosXo2(x);
+		pan.setPosYo2(y);
+		frame.repaint();
+		repaint();
+
 	}
 
 	// Equation d'une parabole
 	private float Parabole(float x, float y) {
-		y = (float) (-((0.005) * (x * x)) + 4 * x + 2);
+		y = frame.getHeight() - (float) (-((0.005) * (x * x)) + 4 * x + 2);
 		return y;
 	}
 
