19faaff9147977620a3aba6f49eed41fba2e9127
RenowxSiders <renaud.carpentier@etudiant.univ-lille.fr>
2015-11-01 17:11:03 +0100

    refactoring

--- src/tests/RenaudTest2.java
+++ src/tests/RenaudTest2.java
@@ -1,6 +1,7 @@
 package tests;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -13,42 +14,45 @@ public class RenaudTest2 extends JFrame {
 
 	private Panneau pan = new Panneau();
 
-	JFrame frame = new JFrame();
 	Timer time = new Timer();
 	int x = pan.getPosX();
 	int y = pan.getPosY();
-	static ArrayList<Point> liste = new ArrayList<>();
+	static List<Point> liste = new ArrayList<>();
 	int i = 0;
 
 	public static void main(String args[]) {
+
 		Random rand2 = new Random();
 		int sqrt = rand2.nextInt(2) + 1;
 
 		Equation eq = new Equation(sqrt);
 		int cpt = 0;
-		Random rand = new Random();
+		rand2 = new Random();
 
-		float k = rand.nextFloat() * 3 + 1;
+		float k = rand2.nextFloat() * 3 + 1;
 
 		for (double t = 0; t <= 15; t = t + 0.001) {
 			liste.add(new Point(eq.f(t), eq.g(t, k)));
 			System.out.println(liste.get(cpt).toString());
 			cpt++;
 		}
+
 		new RenaudTest2();
 		System.out.println("sqrt : " + sqrt);
 		System.out.println("coef : " + k);
 	}
 
 	public RenaudTest2() {
-		frame.setTitle("Angry_Bird");
-		frame.setSize(1200, 710);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
-		frame.setContentPane(pan);
-		frame.setVisible(true);
+		this.setTitle("Angry_Bird");
+		this.setSize(1200, 710);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setContentPane(pan);
+		this.setVisible(true);
+		
 		int delay = 10; // milliseconds
+		
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
@@ -62,15 +66,15 @@ public class RenaudTest2 extends JFrame {
 
 	private void go() {
 		if (i < liste.size()
-				&& ((600 - liste.get(i).getY()) <= frame.getHeight()-25)
+				&& ((600 - liste.get(i).getY()) <= this.getHeight() - 25)
 				&& ((600 - liste.get(i).getY()) > 1)
-				&& (liste.get(i).getX() < frame.getWidth()-25)) {
+				&& (liste.get(i).getX() < this.getWidth() - 25)) {
 			x = (int) (liste.get(i).getX() * 100);
 			y = (int) (600 - liste.get(i).getY());
 			pan.setPosX(x);
 			pan.setPosY(y);
 
-			frame.repaint();
+			this.repaint();
 			repaint();
 			i = i + 35;
 		}
