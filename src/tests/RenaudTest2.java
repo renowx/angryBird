0645baab030c849901012bde1bbb6e71d288e712
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2015-11-01 23:33:12 +0100

    Update RenaudTest2
    ajout du mouvement du bec en fonction de la dérivée

--- src/tests/RenaudTest2.java
+++ src/tests/RenaudTest2.java
@@ -1,7 +1,6 @@
 package tests;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,50 +13,47 @@ public class RenaudTest2 extends JFrame {
 
 	private Panneau pan = new Panneau();
 
+	JFrame frame = new JFrame();
 	Timer time = new Timer();
 	int x = pan.getPosX();
 	int y = pan.getPosY();
-	static List<Point> liste = new ArrayList<>();
-	static List<Point> liste2 = new ArrayList<>();
-
+	static ArrayList<Point> liste = new ArrayList<>();
+	static ArrayList<Point> listeBec = new ArrayList<>();
 	int i = 0;
 
 	public static void main(String args[]) {
-
-		Random rand = new Random();
-		int sqrt = rand.nextInt(2) + 1;
+		Random rand2 = new Random();
+		int sqrt = rand2.nextInt(2) + 1;
 
 		Equation eq = new Equation(sqrt);
 		int cpt = 0;
-		rand = new Random();
-		
-		float k = rand.nextFloat() * 3 + 1;
+		Random rand = new Random();
 
+		float k = rand.nextFloat() * 3 + 1;
+		
 		for (double t = 0; t <= 15; t = t + 0.001) {
 			liste.add(new Point(eq.f(t), eq.g(t, k)));
-			liste2.add(new Point(eq.derive(eq.f(t), k), eq.derive(eq.g(t, k), k)));
 			System.out.println(liste.get(cpt).toString());
-			System.out.println(liste2.get(cpt).toString());
+			//liste des points du bec
+			listeBec.add(new Point(eq.f(t), eq.derivG(t, k)));
+			System.out.println(listeBec.get(cpt).toString());
 			cpt++;
 		}
-
+		
 		new RenaudTest2();
 		System.out.println("sqrt : " + sqrt);
 		System.out.println("coef : " + k);
-		
 	}
 
 	public RenaudTest2() {
-		this.setTitle("Angry_Bird");
-		this.setSize(1200, 710);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		this.setContentPane(pan);
-		this.setVisible(true);
-		
+		frame.setTitle("Angry_Bird");
+		frame.setSize(1200, 710);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setContentPane(pan);
+		frame.setVisible(true);
 		int delay = 10; // milliseconds
-		
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
@@ -71,15 +67,19 @@ public class RenaudTest2 extends JFrame {
 
 	private void go() {
 		if (i < liste.size()
-				&& ((600 - liste.get(i).getY()) <= this.getHeight() - 25)
+				&& ((600 - liste.get(i).getY()) <= frame.getHeight()-25)
 				&& ((600 - liste.get(i).getY()) > 1)
-				&& (liste.get(i).getX() < this.getWidth() - 25)) {
+				&& (liste.get(i).getX() < frame.getWidth()-25)) {
 			x = (int) (liste.get(i).getX() * 100);
-			y = (int) (600 - liste.get(i+15).getY());
+			y = (int) (600 - liste.get(i).getY());
+			//mouvement du bec
+			pan.setPosBecX((int) (listeBec.get(i).getX() * 100));
+			pan.setPosBecY((int) (600 - listeBec.get(i).getY()));
+
 			pan.setPosX(x);
 			pan.setPosY(y);
-			
-			this.repaint();
+
+			frame.repaint();
 			repaint();
 			i = i + 35;
 		}
