926788c1d04dcd3721c778773e2a5509e9212cf0
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2015-11-05 19:54:30 +0100

    Update AlexTests
    Copié-collé de la classe RenaudTests2, j'ai gardé la même architecture j'ai juste rajouter mes lignes

--- src/tests/AlexTests.java
+++ src/tests/AlexTests.java
@@ -1,51 +1,97 @@
 package tests;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+
+import modele.AlexPanneau;
+import modele.Panneau;
 
 public class AlexTests extends JFrame {
 
-	public AlexTests() {
-		
-		AlexTests testAlex = new AlexTests();	
-	
-		JFrame frame = new JFrame("Hungry Birds");
-	
-		JPanel pan = new JPanel();
-		
-		JLabel bird = new JLabel("O");
-		
-		pan.setLayout(null);
-		bird.setForeground(Color.BLACK);
-		pan.add(bird);
-						
-	    ImageIcon burd= new ImageIcon("Images/oiseau.png");
-        JLabel boob = new JLabel(burd);
-        boob.setBounds(350, 225, 100, 100);
-        pan.add(boob);
+	static Random rand2 = new Random();
+	static int sqrt = rand2.nextInt(2) + 1;
 
+	static Random rand = new Random();
+
+	public static float k = rand.nextFloat() * 3 + 1;
+	public static Equation eq = new Equation(sqrt, k);
+
+	private AlexPanneau pan = new AlexPanneau();
+
+	JFrame frame = new JFrame();
+	Timer time = new Timer();
+	int x = pan.getPosX();
+	int y = pan.getPosY();
+	static ArrayList<Point> liste = new ArrayList<>();
+	static ArrayList<Point> listeBec = new ArrayList<>();
+	static ArrayList<Point> listeBecDeriv = new ArrayList<>();
+
+	int i = 0;
+
+	public static void main(String args[]) {
+
+		for (double t = 0; t <= 15; t = t + 0.001) {
+			//liste des points de l'oiseau
+			liste.add(new Point(eq.f(t), eq.g(t)));
+			
+			//listes des points du bec
+			listeBec.add(new Point(eq.f(t+0.001*200), eq.g(t+0.001*200)));
+			listeBecDeriv.add(new Point(eq.f(t+0.001*200), eq.tangente(eq.f(t+0.001*75), (t+0.001*75))));
+		}
 		
-        frame.add(pan);
-        frame.setSize(1200, 800);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setResizable(false);
 		
-		
+		new AlexTests();
+		System.out.println("sqrt : " + sqrt);
+		System.out.println("coef : " + k);
 	}
+
+	public AlexTests() {
+		frame.setTitle("Angry_Bird");
+		frame.setSize(1200, 710);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setContentPane(pan);
+		frame.setVisible(true);
+		int delay = 10; // milliseconds 
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				go();
+			}
+		};
+
+		time.schedule(task, delay, 10);
+
+	}
+
+	private void go() {
+		if (i < liste.size()
+				&& ((600 - liste.get(i).getY()) <= frame.getHeight()-25)
+				&& ((600 - liste.get(i).getY()) > 1)
+				&& (liste.get(i).getX() < frame.getWidth()-25)) {
+			x = (int) (liste.get(i).getX() * 100);
+			y = (int) (600 - liste.get(i).getY());
 	
-	public static void main(String[] args) {
-		AlexTests frame = new AlexTests();
+			//mouvement du bec
+			pan.setPosBecX((int) (listeBec.get(i).getX() * 100));
+			pan.setPosBecY((int) (600 - listeBec.get(i).getY()));
+			pan.setPosBecDX((int) (listeBecDeriv.get(i).getX() * 100));
+			pan.setPosBecDY((int) (600 - listeBecDeriv.get(i).getY()));
+			
+			//mouvement de l'oiseau
+			pan.setPosX(x);
+			pan.setPosY(y);
+
+			frame.repaint();
+			repaint();
+			i = i + 35;
+		}
+
 	}
 
 }
