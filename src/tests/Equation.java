30de24ef485f9b82f9dd1350141e8760d63f18f0
Ludo <ludovic.ouvry@aliceadsl.fr>
2015-11-06 00:40:41 +0100

    Ajouts des méthode avec k

--- src/tests/Equation.java
+++ src/tests/Equation.java
@@ -1,17 +1,39 @@
 package tests;
 
+
 public class Equation {
 	private int coef;
 	private float k;
-
-	public Equation(int sqrt, float k) {
+	public Equation(int sqrt){
+		this.coef = sqrt;
+	}
+	public double f(double t){
+		return t;
+	}
+	
+	public double g(double t, float k){
+		
+		return k*Math.sin(0.3*t*coef)*200;
+		
+	}
+	//calcul de la dérivée
+	public double derivG(double t, float k) {
+		return 60*k*coef*Math.cos((3*coef*t/10));
+	}
+	
+	public double derive(double t, float k){
+		return 0.3*Math.cos(0.3*t*coef)*200;
+	}
+	
+	public int getSqrt(){
+		return coef;
+	}
+        public Equation(int sqrt, float k) {
 		this.coef = sqrt;
 		this.k = k;
 	}
 
-	public double f(double t) {
-		return t;
-	}
+	
 
 	public double g(double t) {
 
@@ -27,8 +49,7 @@ public class Equation {
 
 		return derive(t) * (x - t) + g(t);
 	}
-
-	public int getSqrt() {
-		return coef;
-	}
+        
+      
+	
 }
