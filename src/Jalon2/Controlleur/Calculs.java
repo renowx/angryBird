d5c1d6b3bf7ba9284600b370f6d0b8a7d642af9c
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2015-12-12 10:45:52 +0100

    Update Calculs.java
    Ajout de la première Javadoc [Révisions à effectuer]

--- src/Jalon2/Controlleur/Calculs.java
+++ src/Jalon2/Controlleur/Calculs.java
@@ -16,18 +16,39 @@ public class Calculs {
     double timeIncrement = totalTime / pas;
     double xIncrement = xV * timeIncrement;
 	
+	/**
+	 * Calcul la distance entre 2 points, dont les coordonnees sont passées en parametres
+	 * @param int, int, int, int
+	 * @return int
+	 **/
 	public int distance (int x1, int y1, int x2, int y2){
 		int dist = (int) Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
 		return dist;
 	}
 	
+	/**
+	 * Prend un int en parametre pour le renvoyer en double
+	 * @param int
+	 * @return double
+	 **/
 	public double y(int t){
 		return t;
 	}
+	
+	/**
+	 * Effectue un calcul à partir de l'entier t. Renvoie un double
+	 * @param int
+	 * @return double
+	 **/
 	public double yP(int t){
 		return 10-((0.07)*(y(t)));
 	}
 	
+	/**
+	 * Renvoie un point approximatif de la courbe en affichant les donnees traitees
+	 * @param int, float, int
+	 * @return Point
+	 **/
 	public Point aproxCourbe(int x, float y, int t){
         
         y = (float) (yV * t + 0.5 * ACCELERATION * t * t);
@@ -36,10 +57,20 @@ public class Calculs {
         return new Point(x+1, (int) y);
 	}
 	
+	/**
+	 * Renvoie le calcul du point n+1
+	 * @param double
+	 * @return double
+	 **/
 	public double round2(double n) {
         return (int) (n * 100.0 + 0.5) / 100.0;
     }
 	
+	/**
+	 * Calcul l'angle, en radian, entre 2 points
+	 * @param Point, Point
+	 * @return double
+	 **/
 	public double angle(Point a, Point b){ 
 		double distb = b.getX() - a.getX();
 		double distc = b.getY() - a.getY();
