3a862809bb1968a899a00fc49054213ff745cf59
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2015-12-13 11:17:39 +0100

    Update javadoc Transformation.java
    Reformulation de la javadoc déjà présente pour certaines phrases :)  ("coin supérieur gauche" c'est plus joli que "en haut à gauche" ;) )

--- src/Jalon2/Controlleur/Transformation.java
+++ src/Jalon2/Controlleur/Transformation.java
@@ -8,10 +8,12 @@ package Jalon2.Controlleur;
 import Jalon2.Vue.Scene;
 
 /**
- * Classe qui permet de passer le repère de en haut à gauche à en bas à gauche.
- * Attention valeur mis en dur pour la taille de la fenètre à changer si les valeurs change.
+ * Classe qui permet de passer le repère du coin supérieur gauche au coin inférieur gauche.
+ * 
+ * <B>Attention valeur mis en dur pour la taille de la fenètre à changer si les valeurs change.</B>
+ * 
  * @author Ludovic
- */
+ **/
 public  class Transformation {
     
    
@@ -21,20 +23,20 @@ public  class Transformation {
     } 
     
     /**
-     *Recupère la coordonée y d'un point et la passe dans le repère en bas à gauche.
-     * @param y La coordonée en y du point
-     * @return la nouvelle coordonée en y.
-     */
+     * Recupère la coordonée y d'un point et la passe dans le repère du coin inférieur gauche.
+     * @param int y La coordonée en y du point
+     * @return int la nouvelle coordonée en y.
+     **/
     public static int passageRepereBG(int y){
          return 710-y;
      }
      
     /**
      *
-     * Recupère la coordonée y d'un point et la passe dans le repère en haut à gauche.
-     * @param y La coordonée en y du point
-     * @return la nouvelle coordonée en y.
-     */
+     * Recupère la coordonée y d'un point et la passe dans le repère du coin supérieur gauche.
+     * @param int y La coordonée en y du point
+     * @return int la nouvelle coordonée en y.
+     **/
     public static int passageRepereHG(int y){
          return 710-y;
      }
