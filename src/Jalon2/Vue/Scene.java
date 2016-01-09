49e91a34ff23ebd434738f3107c75d58aeaaa5ee
Ludo <ludovic.ouvry@aliceadsl.fr>
2016-01-09 14:32:27 +0100

    Ancienne scene remise

--- src/Jalon2/Vue/Scene.java
+++ src/Jalon2/Vue/Scene.java
@@ -1,30 +1,25 @@
+
+
 package Jalon2.Vue;
+
+import Jalon2.Controlleur.Controlleur;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JFrame;
 
-import Jalon2.Controlleur.Controlleur;
 import Jalon2.Modele.Bird;
 import Jalon2.Modele.Modele;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static Jalon2.Controlleur.Transformation.passageRepereBG;
+import static Jalon2.Controlleur.Transformation.passageRepereHG;
+
 
 /**
- * Sera la vue, qui affichera l'oiseau et les obstacles en fonction des données
- * contenues dans modele.
- * 
+ * Sera la vue, qui affichera l'oiseau et les obstacles en fonction des données contenues  dans modele.
  * @author ouvryl
  */
-<<<<<<< HEAD
-public class Scene extends JFrame {
-	Modele modele;
-	private Bird b = new Bird(100, 500, 50);
-	static Scene s;
-	BirdView bView;
-	Controlleur c;
-
-	public Scene(Modele modele, Controlleur c) {
-		bView = new BirdView(b, modele, c);
-		this.modele = modele;
-		this.c = c;
-=======
 public class Scene extends JFrame{
         Modele modele;
 	private static Bird b = new Bird(100, 500, 50);
@@ -38,7 +33,6 @@ public class Scene extends JFrame{
                 bView= new BirdView(b, modele,c);
                 this.modele=modele;
                 this.c=c;
->>>>>>> origin/master
 		this.setTitle("Angry_Bird");
 		this.setSize(1200, 710);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -47,12 +41,11 @@ public class Scene extends JFrame{
 		this.setVisible(true);
 		this.setContentPane(bView);
 		this.setVisible(true);
-
+                
+                
 	}
-
+	
 	public static void main(String args[]) {
-<<<<<<< HEAD
-=======
            
                 
                 Modele modele= new Modele();
@@ -65,36 +58,17 @@ public class Scene extends JFrame{
                         try {
                             Thread.sleep(100);
                         } catch (InterruptedException ex) {
->>>>>>> origin/master
 
-		Modele modele = new Modele();
-		Controlleur c = new Controlleur(modele);
-		s = new Scene(modele, c);
-		// impossible de lancer letsGo dans la classe mouse relased car cette
-		// classe ne rend pas la
-		// main sur l'affichage.
-		while (true) {
-			if (!modele.collision()) {
-				if (s.bView.mouseRelease) {
-					s.bView.controlleur.letsGo();
-					// s.bView.mouseRelease = false;
-				}
+                        }
+                    }
 
-				while (!s.bView.mouseRelease) {
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException ex) {
-
-					}
-				}
-
-				s.bView.controlleur.letsGo();
-				// s.bView.mouseRelease = false;
-			} else {
-				break;
-			}
-		}
-
+                    s.bView.controlleur.letsGo();
+                    s.bView.mouseRelease=false;
+                }
+             
+                
 	}
 
+	
+	
 }
